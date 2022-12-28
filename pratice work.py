6148b0518ce41fc0d472b518a6564485c075de6f
johnson vj <121477725+samplejo@users.noreply.github.com>
2022-12-28 18:11:07 +0530

    Add files via upload

---
+++ pratice work.py
@@ -0,0 +1,5 @@
+name="Johnson vj"
+age=24
+print("Hello {} your age is {}".format(name,age))
+print("Hello {1}your age is {0}".format(age,name))
+print("Hello {name} your age is {age}".format(name="johnson vj",age=24))
