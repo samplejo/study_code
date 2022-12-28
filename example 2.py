452d9bd3e67f7a8428514fdd963363f6b5f9186e
johnson vj <121477725+samplejo@users.noreply.github.com>
2022-12-28 18:08:47 +0530

    Add files via upload

---
+++ example 2.py
@@ -0,0 +1,13 @@
+name ="Ram"
+age = 22
+city="banglore"
+print(name,age)
+print("Name is: ",name)
+print("Age is : ",age)
+print("Hi {} your age is {}".format(name,age))
+print("Hi {name} welcome for {course}".format(name="mary",course="python"))
+print(f" Welcome {name} to {city}")
+a=22
+b=44
+print(f"Addition of {a} and {b} is {a+b}")
+print(f"Datatype of a is {type(a)}")
